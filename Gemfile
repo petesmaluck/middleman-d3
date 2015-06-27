f6ab37bf29ce9ecfc8da95e6e56545c1d2376d15
Pete Smaluck <petesmaluck@gmail.com>
2015-06-27 18:12:15 -0400

    Added config files

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source 'https://rubygems.org'
 
 gem "middleman", "~>3.3.3"
 
+gem "middleman-neat"
+
 # Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
 
--- config.rb
+++ config.rb
@@ -63,6 +63,8 @@ set :images_dir, 'images'
 
 set :data_dir, 'data'
 
+activate :neat
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
